41e04d5d103b1ba58965c1e90d7ede47f46667a2
samukelok <samkelokay2@gmail.com>
2025-09-07 21:18:46 +0200

    feat(resources): add ProductImageResource for API responses

---
+++ app/Http/Resources/ProductImageResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductImageResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'product_id' => $this->product_id,
+            'filename' => $this->filename,
+            'url' => $this->url,
+            'order' => $this->order,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
